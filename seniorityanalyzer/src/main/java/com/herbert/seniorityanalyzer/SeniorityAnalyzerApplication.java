35900cfb39eddec9f8bc5b5ea8fb693d1cc3b09e
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2026-01-15 08:41:04 -0400

    Spring Project Created

---
+++ seniorityanalyzer/src/main/java/com/herbert/seniorityanalyzer/SeniorityAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.herbert.seniorityanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorityAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorityAnalyzerApplication.class, args);
+	}
+
+}
